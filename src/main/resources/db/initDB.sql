327126f9e848f10cb2c0fec1db07da8dc5e661ca
Azatick94 <azburhanov@mail.ru>
2021-02-21 22:28:06 +0300

    HW3 db_queries_update

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS meals;
+
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -23,3 +25,17 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id          SERIAL PRIMARY KEY,
+    datetime    TIMESTAMP    NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    calories    INTEGER      NOT NULL,
+    user_id     INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX datetime_unique_each_user_id ON meals (datetime, user_id);
+
+
+
